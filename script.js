b18b0165b7144c45383f3586467b6f4f95a73c9c
Rajeev Roy <109815313+RajeevRoy120@users.noreply.github.com>
2025-03-18 21:49:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.querySelector("input[type='button']").addEventListener("click", function () {
+    // Get the dropdown list
+    const select = document.getElementById("colorSelect");
+
+    // Get the selected option
+    const selectedOption = select.options[select.selectedIndex];
+
+    // Remove the selected option if it exists
+    if (selectedOption) {
+        select.remove(select.selectedIndex);
+    }
+});
